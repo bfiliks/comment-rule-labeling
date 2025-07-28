0d44183a8838d3abeadd477ffeb4195f2b506003
Felix Oke <bfiliks4xt@gmail.com>
2025-07-28 05:09:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import json
 import datetime
 from oauth2client.service_account import ServiceAccountCredentials
 
-st.set_page_config(page_title="Google Sheets Sync Labeling Tool", page_icon="favicon.png", layout="wide")
-st.title("🧠 Comment Rule Labeling Tool (Google Sheets Sync)")
+st.set_page_config(page_title="YouTube Comment Labeling Tool", page_icon="favicon.png", layout="wide")
+st.title("Comment Rule Labeling Tool")
 
 # Sidebar instructions
 with st.sidebar.expander("📝 Annotator Instructions", expanded=True):
