99c37b35d66a72598bfed895fe23fcae22d0310a
Felix Oke <bfiliks4xt@gmail.com>
2025-07-28 03:35:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import datetime
 from oauth2client.service_account import ServiceAccountCredentials
 
 st.set_page_config(page_title="Google Sheets Sync Labeling Tool", page_icon="favicon.png", layout="wide")
-st.title("Comment Rule Labeling Tool (Google Sheets Sync via Secrets)")
+st.title("Comment Rule Labeling Tool")
 
 # Annotator login
 annotator = st.sidebar.text_input("Enter your name to begin:").strip().lower()
